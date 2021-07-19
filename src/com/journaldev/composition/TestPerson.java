0b55caa7d8c8e6b6638659d289d97f7ce9408a49
ntpoom9 <80139353+ntpoom9@users.noreply.github.com>
2021-07-19 16:43:36 +0700

    salary

--- src/com/journaldev/composition/TestPerson.java
+++ src/com/journaldev/composition/TestPerson.java
@@ -5,6 +5,7 @@ public class TestPerson {
 		public static void main(String[] args) {
 	        Person person = new Person();
 	        long salary = person.getSalary();
+	        System.out.println("Salary is "+salary);
 	    }
 
 }
