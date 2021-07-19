c6d08d44e8efdd5e45096ad772e08b1f941af182
ntpoom9 <80139353+ntpoom9@users.noreply.github.com>
2021-07-19 15:58:52 +0700

    Initial commit

---
+++ src/com/journaldev/composition/Person.java
@@ -0,0 +1,14 @@
+package com.journaldev.composition;
+
+public class Person {
+	
+	private Job job;
+	   
+    public Person(){
+        this.job=new Job();
+        job.setSalary(1000L);
+    }
+    public long getSalary() {
+        return job.getSalary();
+    }
+}
